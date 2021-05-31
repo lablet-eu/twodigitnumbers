c2428a6d33bae36a4c283c7fd14ce3f621c4396a
lablet-eu <85132881+lablet-eu@users.noreply.github.com>
2021-05-31 22:57:08 +0200

    main.ts, test.ts frissítése

--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
-namespace TwoDigitNumbers
+namespace TwoDigitNumbers{
+    let numbers=[
+        [[0,1,0,0,0],[1,1,1,1,1]],
+        [[1,0,1,1,1],[1,1,1,0,1]],
+        [[0,1,0,0,0],[1,1,1,1,1]],
+        [[0,1,0,0,0],[1,1,1,1,1]],
+        [[0,1,0,0,0],[1,1,1,1,1]],
+        [[0,1,0,0,0],[1,1,1,1,1]],
+        [[0,1,0,0,0],[1,1,1,1,1]],
+        [[0,1,0,0,0],[1,1,1,1,1]],
+        [[0,1,0,0,0],[1,1,1,1,1]],
+        [[0,1,0,0,0],[1,1,1,1,1]]]
+    export function show(value:number){
+        TwoDigitNumbers.getImage(value)
+    }
+    export function getImage(value:number):Image{
+        let img:Image
+        let t=Math.floor(value/10)
+        let f=value-t*10
+        copyDigit(0,t,img)
+        copyDigit(3,f,img)
+        return img
+    }
+    function copyDigit(offset:number,digit:number,img:Image){
+        for(let column=0;column<2;column++){
+            for(let row=0;row<5;row++){
+                img.setPixel(offset+column, row, numbers[digit][column][row]==1)
+            }
+        }
+    }
+}
